e50ebbb3e1d812161b4d8a0b16265d17ee25cd74
finchmgn <ffinch@mail.ru>
2019-01-26 00:28:04 +0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path  =                       require('path')
+const webpack =                     require('webpack')
+const HtmlWebpackPlugin =           require('html-webpack-plugin')
+const MiniCssExtractPlugin =        require("mini-css-extract-plugin")
+const CleanWebpackPlugin =          require('clean-webpack-plugin')
+const OptimizeCssAssetsPlugin =     require('optimize-css-assets-webpack-plugin')
+
+
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        //publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },  "css-loader",
+                        "sass-loader"
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img',
+                            useRelativePath: true
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 70
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new CleanWebpackPlugin([
+            './dist/*.*'
+        ]),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            jquery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default', {
+                    discardComments: {
+                        removeAll: true
+                    }
+                }]
+            },
+            canPrint: true
+        })
+
+    ]
+}
