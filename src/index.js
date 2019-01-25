6ab419472a109215c616b3fd8a9dbb5c617c3be0
finchmgn <ffinch@mail.ru>
2019-01-26 00:27:59 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap'
+
+import './scss/main.scss'
+import 'bootstrap/scss/bootstrap.scss'
+
+jQuery(document).ready(function($) {
+    //$('body').css('background-color', 'orange');
+});
